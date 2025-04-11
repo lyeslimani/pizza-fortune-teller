9fc53b3bdb88d33742f5fcf7fb18958e20f89191
Abdallah Slimane <slimane.abdallah75@gmail.com>
2025-04-11 14:25:34 +0200

    edit offset

--- src/main/scala/PizzaFortuneTeller.scala
+++ src/main/scala/PizzaFortuneTeller.scala
@@ -87,7 +87,7 @@ object PizzaFortuneTeller {
     val newStructureDfFormatted = newStructuredDf.withColumn("total_price_sum", format_number(col("total_price_sum"), 2))
       .orderBy(col("parsed_order_date").asc)
 
-    newStructureDfFormatted.show(false)
+    newStructureDfFormatted.show(31, false)
 
   }
 }
